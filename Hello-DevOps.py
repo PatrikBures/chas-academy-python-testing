b88353c9d25f38025be9e5dbf56ca951eccb2beb
Patrik Bures <patrikbures5@gmail.com>
2025-09-22 14:50:11 +0200

    updated hello-devops

--- Hello-DevOps.py
+++ Hello-DevOps.py
@@ -1,19 +1,25 @@
-import sys
-
 def try_int(num):
     try:
         int(num)
     except:
-        print("not a int")
-        sys.exit()
-    return int(num)
+        return False
+    return True
+
+def get_int(a):
+    while True:
+        num = input(a)
+        if try_int(num):
+            return int(num)
+        else:
+            print("not an int")
+
 
 name = input("What is your name? ")
 
-print(f"Hello {name}!")
+print(f"Hello {name}! input some ints.")
 
-num1 = try_int(input("num1: "))
-num2 = try_int(input("num2: "))
+num1 = get_int("num1: ")
+num2 = get_int("num2: ")
 
 print(f"{num1} * {num2} = {num1 * num2}")
 
